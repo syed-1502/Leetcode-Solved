4ed1e5f8159f59246efb24623f40505a548aa0be
syed-1502 <aurangpasha786@gmail.com>
2026-08-29 15:22:36 +0530

    Time: 479 ms (36.39%) | Memory: 0B (100.00%) - LeetSync

---
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select
+ p.firstName,
+ p.lastName,
+ a.city,
+ a.state
+from Address a 
+right join Person p 
+on p.personId=a.personId;
